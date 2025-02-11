bf99d4dd495c7887f1ca8deeb396faca48f418b8
Manish <106720687+M1973n@users.noreply.github.com>
2025-02-11 21:03:58 +0530

    Initial commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://netfflix-devmaniya-db8089.netlify.app',
+    origin: 'https://blackflix-devmaniya-db8089.netlify.app',
     credentials: true
 }));
 
